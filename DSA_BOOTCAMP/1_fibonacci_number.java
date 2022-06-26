7a5d1b92bd1b6895b0b4a179fe2625acbaef67a1
Ayush Gangwar <ayush.gangwar210899@gmail.com>
2022-06-26 19:29:22 +0530

    fibonacci number

---
+++ DSA_BOOTCAMP/1_fibonacci_number.java
@@ -0,0 +1,17 @@
+public static int solve(int n){
+    //CODE HERE 
+     if(n==0)
+        {
+            return 0;
+        }
+        if(n==1)
+        {
+            return 1;
+        }
+        else
+        {
+            return solve(n-1)+solve(n-2);
+    }
+    } 
+
+
